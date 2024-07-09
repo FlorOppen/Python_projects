0e8fda874f2f8b5f0ce5796e804f65b3382b286a
Florencia Oppenheimer <floroppen@Florencias-MacBook-Air.local>
2024-07-09 18:29:10 +0200

    feat: Replace normalize with StandardScaler in app.py to ensure consistency with train.py and inference.py
    - Updated app.py to use StandardScaler for data preprocessing
    - This change aligns the preprocessing steps in app.py with those in train.py and inference.py
    - Ensured that the scaler is loaded and applied correctly to incoming data in the Flask application

--- Machine_Learning/Wine_project/app.py
+++ Machine_Learning/Wine_project/app.py
@@ -1,20 +1,23 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from joblib import load
-from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
-# Cargar el modelo previamente entrenados
-model_nn = load("Inference_NN.joblib")
+# Cargar el modelo previamente entrenado y el escalador
+model_nn = load("Inference_NN.joblib"
+scaler = load("scaler.joblib")
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
     df = pd.DataFrame(data)
-    df_normalized = preprocessing.normalize(df, norm='l2')
     
-    prediction_nn = model_nn.predict(df_normalized)
+    # Escalar los datos usando StandardScaler
+    df_scaled = scaler.transform(df)
+    
+    prediction_nn = model_nn.predict(df_scaled)
 
     return jsonify({
         'nn_predictions': prediction_nn.tolist()
